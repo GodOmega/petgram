2a6127ca1ce9bd1edc26dd98331daf014f6f4bad
Andrés Gazui <gazuiandres@gmail.com>
2021-05-14 21:52:33 -0400

    removing files

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const WebpackPwaManifestPlugin = require("webpack-pwa-manifest");
-// const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const WebpackPwaManifestPlugin = require("webpack-pwa-manifest");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
@@ -50,51 +50,51 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "assets/[name].css",
     }),
-    // new WebpackPwaManifestPlugin({
-    //   name: "PetGram - Tu app de fotos de mascotas",
-    //   short_name: "Petgram 🐶",
-    //   description: "Con petgram encuentra fotos de tus animales favoritos",
-    //   start_url: "/",
-    //   scope: "/",
-    //   background_color: "#ffffff",
-    //   theme_color: "#b1a",
-    //   crossorigin: null, //can be null, use-credentials or anonymous
-    //   icons: [
-    //     {
-    //       src: path.resolve("src/assets/icon.png"),
-    //       sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
-    //     },
-    //     {
-    //       src: path.resolve("src/assets/icon.png"),
-    //       size: "1024x1024",
-    //       purpose: "maskable",
-    //     },
-    //   ],
-    // }),
-    // new WorkboxWebpackPlugin.GenerateSW({
-    //   swDest: "service-worker.js",
-    //   clientsClaim: true,
-    //   skipWaiting: true,
-    //   maximumFileSizeToCacheInBytes: 5000000,
-    //   runtimeCaching: [
-    //     {
-    //       urlPattern: new RegExp(
-    //         "https://(res.cloudinary.com|images.unsplash.com)"
-    //       ),
-    //       handler: "CacheFirst",
-    //       options: {
-    //         cacheName: "images",
-    //       },
-    //     },
-    //     {
-    //       urlPattern: new RegExp("https://petgram-server-clgg.vercel.app"),
-    //       handler: "NetworkFirst",
-    //       options: {
-    //         cacheName: "api",
-    //       },
-    //     },
-    //   ],
-    // }),
+    new WebpackPwaManifestPlugin({
+      name: "PetGram - Tu app de fotos de mascotas",
+      short_name: "Petgram 🐶",
+      description: "Con petgram encuentra fotos de tus animales favoritos",
+      start_url: "/",
+      scope: "/",
+      background_color: "#ffffff",
+      theme_color: "#b1a",
+      crossorigin: null, //can be null, use-credentials or anonymous
+      icons: [
+        {
+          src: path.resolve("src/assets/icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+        },
+        {
+          src: path.resolve("src/assets/icon.png"),
+          size: "1024x1024",
+          purpose: "maskable",
+        },
+      ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: "service-worker.js",
+      clientsClaim: true,
+      skipWaiting: true,
+      maximumFileSizeToCacheInBytes: 5000000,
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            "https://(res.cloudinary.com|images.unsplash.com)"
+          ),
+          handler: "CacheFirst",
+          options: {
+            cacheName: "images",
+          },
+        },
+        {
+          urlPattern: new RegExp("https://petgram-server-clgg.vercel.app"),
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "api",
+          },
+        },
+      ],
+    }),
   ],
   devServer: {
     contentBase: path.join(__dirname, "dist"),
